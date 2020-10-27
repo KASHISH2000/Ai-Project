2e6cee8f2c09b2fe3f63917515695c06fd465c85
Kashish Mittal <kashkind7@gmail.com>
2020-10-27 11:51:25 +0530

    first

--- policyGradientNetwork.py
+++ policyGradientNetwork.py
@@ -20,8 +20,8 @@ class Network:
         self.sampled_actions = tfc.placeholder(tfc.float32, [None, 1])
         self.advantage = tfc.placeholder(tfc.float32, [None, 1], name='advantage')
         # hidden layers features
-        hidden_layers = tfc.layers.dense(self.observations, units=hidden_layer_size, activation=tf.nn.relu, kernel_initializer=tfc.contrib.layers.xavier_initializer())
-        self.up_probability = tfc.layers.dense( hidden_layers, units=1, activation=tf.sigmoid, kernel_initializer=tfc.contrib.layers.xavier_initializer())
+        hidden_layers = tfc.layers.dense(self.observations, units=hidden_layer_size, activation=tf.nn.relu, kernel_initializer=tf.contrib.layers.xavier_initializer())
+        self.up_probability = tfc.layers.dense( hidden_layers, units=1, activation=tf.sigmoid, kernel_initializer=tf.contrib.layers.xavier_initializer())
         self.loss = tfc.losses.log_loss(labels=self.sampled_actions, predictions=self.up_probability, weights=self.advantage)
         # set optimizer of network
         optimizer = tf.train.AdamOptimizer(self.learning_rate)
